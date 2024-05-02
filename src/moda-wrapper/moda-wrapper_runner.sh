1ef91be09c3ec8e728a82e50067d669bb27aaacc
Turnyur <chukunah@gmail.com>
2024-05-02 12:36:32 +0200

    update script input file paths

--- src/moda-wrapper/moda-wrapper_runner.sh
+++ src/moda-wrapper/moda-wrapper_runner.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-Rscript ./R/hierarchical-clustering.R ./dataset/Macrophages/ ./dataset/TCells/ output/ -i1 Macrophages -i2 TCells -c 2 -s 0.1 -t 0.1
+Rscript ./R/hierarchical-clustering.R ./R/dataset/Macrophages/ ./R/dataset/TCells/ ./R/output/ -i1 Macrophages -i2 TCells -c 2 -s 0.1 -t 0.1
